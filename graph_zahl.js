8fac8f368109f65ad099f4509722255d63921462
Helge Rausch <helge@rausch.io>
2011-12-04 15:43:26 +0100

    fixing graph filling

---
+++ graph_zahl.js
@@ -0,0 +1,140 @@
+GraphZahl = function(id) {
+
+  this.graphs = [];
+  this.stacked = false;
+  this.canvas = document.getElementById(id);
+  this.context = this.canvas.getContext('2d');
+  this.canvas_width = this.canvas.width;
+  this.canvas_height = this.canvas.height;
+  
+
+
+  this.draw = function() {
+  
+    var tmp, x_min, x_max, y_min, y_max, data, x, y;
+
+    for(var i = 0; i < this.graphs.length; i++)
+      this.graphs[i].data = this.graphs[i].data.sort(function(a,b) a[0] - b[0]);
+
+    if(this.stacked == true)
+      this.graphs = this.stack(this.graphs);
+
+    // sort the grap data and find the min and max values
+    for(var i = 0; i < this.graphs.length; i++) {
+      data = this.graphs[i].data = this.graphs[i].data.sort(function(a,b) a[0] - b[0]);
+      tmp = data[data.length-1][0];
+      if(this.x_max == undefined || this.x_max < tmp)
+        this.x_max = tmp;
+      tmp = data[0][0];
+      if(this.x_min == undefined || this.x_min > tmp)
+        this.x_min = tmp;
+      tmp = data.map(function(e) { return e[1] }).max();
+      if(this.y_max == undefined || this.y_max < tmp)
+        this.y_max = tmp;
+      tmp = data.map(function(e) { return e[1] }).min();
+      if(this.y_min == undefined || this.y_min > tmp)
+        this.y_min = tmp;
+    }
+
+    this.x_span = this.x_max - this.x_min;
+    this.y_span = this.y_max - this.y_min;
+
+  
+    // draw the graphs
+    for(var j = 0; j < this.graphs.length; j++) {
+
+      if(this.graphs[j].fill_color) {
+        this.context.beginPath();
+        this.drawPath(this.graphs[j]);
+	[x,y] = this.scale([ this.graphs[j].data[this.graphs[j].data.length-1][0], 0 ] );
+        this.context.lineTo(x,y);
+	[x,y] = this.scale([ this.graphs[j].data[0][0], 0 ]);
+        this.context.lineTo(x,y);
+        this.context.fillStyle = this.graphs[j].fill_color;
+        this.context.fill();
+      }
+
+      if(this.graphs[j].line_color) {
+        this.context.beginPath();
+        this.drawPath(this.graphs[j]);
+        this.context.strokeStyle = this.graphs[j].line_color;
+        this.context.stroke();
+      }
+    }
+  };
+
+  this.drawPath = function(graph) {
+    var data = graph.data;
+    var data_length = data.length;
+    var x, y;
+
+    for(var i = 0; i < data_length; i++) {      
+      [x,y] = this.scale(data[i]);
+      if(i == 0)
+        this.context.moveTo(x,y);
+      else
+        this.context.lineTo(x,y);
+    }
+  };
+
+  this.scale = function([x,y]) {
+    return [
+      ((x - this.x_min) * this.canvas_width) / this.x_span,
+      this.canvas_height - ((y - this.y_min) * this.canvas_height) / this.y_span
+    ];
+  };
+  
+  this.stack = function(graphs) {
+    var upper_graph, lower_graph, x, y, k;
+    var stacked_graphs = [graphs[0]];
+  
+    for(var i = 1; i < graphs.length; i++) {
+      upper_graph = graphs[i].data;
+      lower_graph = graphs[i-1].data;
+      stacked_graphs.push(graphs[i]);
+      stacked_graphs[stacked_graphs.length-1].data = [];
+      for(var j = 0; j < upper_graph.length; j++) {
+        x = upper_graph[j][0];
+        k = 0;
+        while(true) {
+          if(lower_graph.length <= j+k) {
+            y = lower_graph[lower_graph.length-1][1] + upper_graph[j][1];
+            break;
+          } else if(j+k < 0) {
+            y = lower_graph[0][1] + upper_graph[j][1];
+            break;
+          } else if(lower_graph[j+k] != undefined && x == lower_graph[j+k][0]) {
+            y = lower_graph[j+k][1] + upper_graph[j][1];
+            break;
+          } else if(lower_graph[j+k+1] != undefined && x == lower_graph[j+k+1][0]) {
+            y = lower_graph[j+k+1][1] + upper_graph[j][1];
+          } else {
+            if(x > lower_graph[j+k][0]) {
+              if(x < lower_graph[j+k+1][0]) {
+                y = (x - lower_graph[j+k][0]) * (lower_graph[j+k+1][1] - lower_graph[j+k][1]) / (lower_graph[j+k+1][0] - lower_graph[j+k][0]) + lower_graph[j+k][1] + upper_graph[j][1];
+                break;
+              } else {
+                k++;
+              }
+            } else {
+              k--;
+            }
+          }
+        }
+        stacked_graphs[i].data.push([x, y]);
+      }
+    }
+    return stacked_graphs;
+  };
+
+};
+
+Array.prototype.max = function () {
+  return Math.max.apply(Math, this);
+};
+
+Array.prototype.min = function () {
+  return Math.min.apply(Math, this);
+};
+
+
--- spec/GraphZahlSpec.js
+++ spec/GraphZahlSpec.js
@@ -1,13 +1,18 @@
 describe("GraphZahl", function() {
-  var player;
-  var song;
 
   beforeEach(function() {
-
+    var canvas = document.getElementById('graph');
+    if(!canvas) {
+      var canvas = document.createElement("canvas");
+      canvas.id = 'graph';
+      canvas.width = 200;
+      canvas.height = 100;
+      document.body.appendChild(canvas);
+    }
   });
 
   it("should stack graphs", function() {
-    var graph_zahl = new GraphZahl('dummy');
+    var graph_zahl = new GraphZahl('graph');
     var graphs = [{ data: [[1,1],[2,3],[3,2]] },
                   { data: [[1,1],[2,0],[3,2]] }];
     var stacked_graphs = graph_zahl.stack(graphs);
@@ -17,7 +22,7 @@ describe("GraphZahl", function() {
   });
 
   it("should stack graphs with unequal x values", function() {
-    var graph_zahl = new GraphZahl('dummy');
+    var graph_zahl = new GraphZahl('graph');
     var graphs = [{ data: [     [1,1],  [2,3],  [3,2]     ] },
                   { data: [[0.5,1],[1.5,0],[2.5,2],[3.5,1]] }];
     var stacked_graphs = graph_zahl.stack(graphs);
@@ -26,4 +31,17 @@ describe("GraphZahl", function() {
        { data: [[0.5,2],[1.5,2],[2.5,4.5],[3.5,3]] }]);
 
   });
+
+  it("should scale to the canvas dimensions", function() {
+    var graph_zahl = new GraphZahl('graph');
+    graph_zahl.x_min = 100;
+    graph_zahl.x_max = 200;
+    graph_zahl.y_min = 50;
+    graph_zahl.y_max = 100;
+    graph_zahl.x_span = graph_zahl.x_max - graph_zahl.x_min;
+    graph_zahl.y_span = graph_zahl.y_max - graph_zahl.y_min;
+
+    expect(graph_zahl.scale([125,90])).toEqual([50,20]);
+  });
+
 });
